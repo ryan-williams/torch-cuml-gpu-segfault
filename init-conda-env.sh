9d1a84cf332691821db6fae939dfff8ee3730496
Ryan Williams <ryan@runsascoded.com>
2023-04-07 23:14:49 -0400

    conda init fixes

--- init-conda-env.sh
+++ init-conda-env.sh
@@ -9,13 +9,11 @@ wget -q "https://repo.anaconda.com/miniconda/Miniconda3-py39_23.1.0-1-Linux-x86_
 rm ~/miniconda.sh
 echo ". $d/etc/profile.d/conda.sh" >> ~/.bashrc
 . ~/.bashrc
-conda --version
-conda activate base
-conda --version
-time conda install -y -n base conda-libmamba-solver
-conda config --set solver libmamba
-conda config --set channel_priority flexible  # https://github.com/rapidsai/cuml/issues/4016
+conda="$d/bin/conda"
+time "$conda" install -y -n base conda-libmamba-solver
+"$conda" config --set solver libmamba
+"$conda" config --set channel_priority flexible  # https://github.com/rapidsai/cuml/issues/4016
 
 # Create conda env with necessary dependencies (see environment.yml)
-time conda env update -n segfault
+time "$conda" env update -n segfault
 echo "conda activate segfault" >> ~/.bashrc
