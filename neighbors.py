14a96139062cc8c32d07e06e7b28cfcb18b6bcc0
Ryan Williams <ryan@runsascoded.com>
2023-04-08 13:00:28 -0400

    support in-process testing

--- neighbors.py
+++ neighbors.py
@@ -1,13 +1,47 @@
 #!/usr/bin/env python
 import faulthandler
+import sys
+from subprocess import CalledProcessError
+
+import click
+
 faulthandler.enable()
 
 import numpy as np
 
+
+def repeat(fn, n, log=print, fail_msg='segfault', exit_early=False):
+    fmt = f"%0{len(str(n))}d"
+    successes, failures = 0, 0
+    for i in range(n):
+        ii = fmt % (i + 1)
+        log(f"Iteration {ii}/{n}")
+        try:
+            fn()
+            print(f"✅ Success (iteration {ii}/{n})")
+            successes += 1
+        except CalledProcessError as e:
+            if e.returncode in [-11, 139]:  # segfault
+                print(f"❌ Failure (iteration {ii}/{n}): exit code {e.returncode} ({fail_msg})")
+                log(f"{e}")
+                if exit_early:
+                    raise
+                failures += 1
+            elif e.returncode != 1:
+                raise RuntimeError(f"Unexpected returncode {e.returncode}")
+
+    if failures:
+        pct_str = "%.1f" % (failures / n * 100)
+        print(f"❌ {failures}/{n} runs failed ({pct_str}%)")
+        sys.exit(1)
+    else:
+        print(f"✅ all {successes} runs succeeded")
+        sys.exit(0)
+
+
 def neighbors(X):
     # ⚠️️⚠️ This (theoretically unused) import, when run before the cuml import below it, causes the pipeline to segfault
     # on ≈10% of runs. ⚠️⚠️
-    import torch
 
     # This block is adapted from scanpy.neighbors.compute_neighbors_rapids:
     # https://github.com/scverse/scanpy/blob/1.8.2/scanpy/neighbors/__init__.py#L318-L338
@@ -19,14 +53,24 @@ def neighbors(X):
     print(f'knn_indices ({type(knn_indices)}, {knn_indices.shape}): {knn_indices.nonzero()}, knn_distances ({type(knn_distances)}, {knn_distances.shape}): {knn_distances.nonzero()}')
 
 
-def run(random_seed=123, shape='10x2'):
+quiet_opt = click.option('-q', '--quiet', is_flag=True, help='Suppress subprocess output, only print success/failure info for each iteration')
+random_seed_opt = click.option('-r', '--random-seed', default=123, type=int, help='Random seed (set before `cuml.NearestNeighbors` calculation)')
+shape_opt = click.option('-s', '--shape', default='10x2', help='Shape for random array passed to `cuml.NearestNeighbors`')
+
+
+@click.command()
+@quiet_opt
+@random_seed_opt
+@shape_opt
+def run(*, quiet, random_seed, shape):
+    log = (lambda msg: None) if quiet else print
     np.random.seed(random_seed)
     R, C = shape.split('x', 1)
     R, C = int(R), int(C)
     X = np.random.random((R, C))
-    print(f"Running cuml NearestNeighbors.kneighbors on random {R}x{C} array")
+    log(f"Running cuml NearestNeighbors.kneighbors on random {R}x{C} array")
     neighbors(X)
-    print("Neighbors complete")
+    log("Neighbors complete")
 
 
 if __name__ == '__main__':
--- run.py
+++ run.py
@@ -1,52 +1,63 @@
 #!/usr/bin/env python
 import shlex
-import sys
-from subprocess import check_call, CalledProcessError, DEVNULL
+from functools import partial
+from subprocess import check_call, DEVNULL
 
 import click
+
+from neighbors import repeat, run, quiet_opt, random_seed_opt, shape_opt
 
 
 @click.command('run.py', help='Repeatedly run `entrypoint.sh`, either in Docker on or the host')
 @click.option('-d', '--docker-img', help="Run this docker image (assumed to have been built from this repo, with ENTRYPOINT `entrypoint.sh`")
-@click.option('-n', '--num-repetitions', 'n', default=30, help='Repeat the `pipeline.py` execution this many times')
+@click.option('-i', '--repeat-in-process', is_flag=True, help='Repeat `cuml.NearestNeighbors` within one process (as opposed to each one in a separate Python subprocess invocation)')
+@click.option('-n', '--num-repetitions', 'n', default=30, help='Repeat `cuml.NearestNeighbors` this many times')
 @click.option('-q', '--quiet', is_flag=True, help='Suppress subprocess output, only print success/failure info for each iteration')
+@quiet_opt
+@random_seed_opt
+@shape_opt
 @click.option('-x', '--exit-early', is_flag=True, help='Exit on first failure')
-def main(docker_img, n, quiet, exit_early):
-    entrypoint = "./neighbors.py"
-    if docker_img:
-        cmd = [ "docker", "run", "-it", "--rm", "--runtime", "nvidia", '-e', entrypoint, docker_img ]
+def main(docker_img, repeat_in_process, n, quiet, random_seed, shape, exit_early):
+    log = (lambda msg: None) if quiet else print
+
+    fail_msg = 'segfault in Docker' if docker_img else 'segfault'
+    if repeat_in_process and not docker_img:
+        repeat(
+            partial(run.callback, quiet=quiet, random_seed=random_seed, shape=shape),
+            n=n,
+            log=log,
+            fail_msg=fail_msg,
+            exit_early=exit_early
+        )
     else:
-        cmd = [entrypoint]
-    call_kwargs = dict(stdout=DEVNULL, stderr=DEVNULL) if quiet else dict()
-    fmt = f"%0{len(str(n))}d"
-    successes, failures = 0, 0
-    log = lambda msg: None if quiet else print
-    for i in range(n):
-        ii = fmt % (i + 1)
-        log(f"Iteration {ii}/{n}")
-        try:
+        run_args = [
+            *(['-q'] if quiet else []),
+            '-r', f'{random_seed}',
+            '-s', shape,
+        ]
+        if docker_img:
+            cmd = [ "docker", "run", "-it", "--rm", "--runtime", "nvidia", ]
+            if repeat_in_process:
+                cmd += [
+                    '--entrypoint', "./run.py",
+                    docker_img,
+                    '-i',
+                    '-n', n,
+                    *run_args,
+                    *(['-x'] if exit_early else []),
+                ]
+            else:
+                cmd += [ docker_img ]
+        else:
+            cmd = ["./neighbors.py", *run_args]
+
+        call_kwargs = dict(stdout=DEVNULL, stderr=DEVNULL) if quiet else dict()
+
+        def fn():
             log(f"Running: {shlex.join(cmd)}")
             check_call(cmd, **call_kwargs)
-            print(f"✅ Success (iteration {ii}/{n})")
-            successes += 1
-        except CalledProcessError as e:
-            if e.returncode in [-11, 139]:  # segfault
-                docker_msg = ' in Docker' if docker_img else ''
-                print(f"❌ Failure (iteration {ii}/{n}): exit code {e.returncode} (segfault{docker_msg})")
-                log(f"{e}")
-                if exit_early:
-                    raise
-                failures += 1
-            else:
-                raise RuntimeError(f"Unexpected returncode {e.returncode}")
 
-    if failures:
-        pct_str = "%.1f" % (failures / n * 100)
-        print(f"❌ {failures}/{n} runs failed ({pct_str}%)")
-        sys.exit(1)
-    else:
-        print(f"✅ runs all succeeded")
-        sys.exit(0)
+        repeat(fn, n=n, log=log, fail_msg=fail_msg, exit_early=exit_early)
 
 
 if __name__ == '__main__':
