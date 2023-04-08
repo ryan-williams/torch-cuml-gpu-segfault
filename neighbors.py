c433221e2a538cc4bbde27e9a9680a8c7d7f58de
Ryan Williams <ryan@runsascoded.com>
2023-04-08 11:30:15 -0400

    -M/--no-metaflow

---
+++ neighbors.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def neighbors(X):
+    # ⚠️️⚠️ This (theoretically unused) import, when run before the cuml import below it, causes the pipeline to segfault
+    # on ≈10% of runs. ⚠️⚠️
+    import torch
+
+    # This block is adapted from scanpy.neighbors.compute_neighbors_rapids:
+    # https://github.com/scverse/scanpy/blob/1.8.2/scanpy/neighbors/__init__.py#L318-L338
+    from cuml.neighbors import NearestNeighbors
+    nn = NearestNeighbors(n_neighbors=X.shape[1], metric='euclidean')
+    X_contiguous = np.ascontiguousarray(X, dtype=np.float32)
+    nn.fit(X_contiguous)
+    knn_indices, knn_distances = nn.kneighbors(X_contiguous)
+    print(f'knn_indices ({type(knn_indices)}, {knn_indices.shape}): {knn_indices.nonzero()}, knn_distances ({type(knn_distances)}, {knn_distances.shape}): {knn_distances.nonzero()}')
+
+
+def run(random_seed=123, shape='10x2'):
+    np.random.seed(random_seed)
+    R, C = shape.split('x', 1)
+    R, C = int(R), int(C)
+    X = np.random.random((R, C))
+    print(f"Running cuml NearestNeighbors.kneighbors on random {R}x{C} array")
+    neighbors(X)
+    print("Neighbors complete")
+
+
+if __name__ == '__main__':
+    run()
--- pipeline.py
+++ pipeline.py
@@ -1,36 +1,9 @@
 import faulthandler
 
-import numpy as np
-
-from metaflow import FlowSpec, Parameter, resources, step
+from metaflow import FlowSpec, Parameter, step
+from neighbors import run
 
 faulthandler.enable()
-
-
-def neighbors(X):
-    # ⚠️️⚠️ This (theoretically unused) import, when run before the cuml import below it, causes the pipeline to segfault
-    # on ≈10% of runs. ⚠️⚠️
-    import torch
-
-    # This block is adapted from scanpy.neighbors.compute_neighbors_rapids:
-    # https://github.com/scverse/scanpy/blob/1.8.2/scanpy/neighbors/__init__.py#L318-L338
-    from cuml.neighbors import NearestNeighbors
-    nn = NearestNeighbors(n_neighbors=X.shape[1], metric='euclidean')
-    X_contiguous = np.ascontiguousarray(X, dtype=np.float32)
-    nn.fit(X_contiguous)
-    knn_indices, knn_distances = nn.kneighbors(X_contiguous)
-    print(f'knn_indices ({type(knn_indices)}, {knn_indices.shape}): {knn_indices.nonzero()}, knn_distances ({type(knn_distances)}, {knn_distances.shape}): {knn_distances.nonzero()}')
-
-
-def run(random_seed=123, shape='10x2'):
-    np.random.seed(random_seed)
-    R, C = shape.split('x', 1)
-    R, C = int(R), int(C)
-    X = np.random.random((R, C))
-    print(f"Running cuml NearestNeighbors.kneighbors on random {R}x{C} array")
-    neighbors(X)
-    print("Neighbors complete")
-
 
 class Pipeline(FlowSpec):
     """Simple Metaflow pipeline exercising the segfaulting `import torch` → `cuml.NearestNeighbors` code path.
@@ -41,7 +14,6 @@ class Pipeline(FlowSpec):
     random_seed = Parameter('random_seed', type=int, default=123)
     shape = Parameter('shape', type=str, default='10x2')
 
-    @resources(memory=200_000, cpu=1, gpu=1)
     @step
     def start(self):
         run(random_seed=self.random_seed, shape=self.shape)
